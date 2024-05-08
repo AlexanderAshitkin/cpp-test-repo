3eb3978b66f9220be9f37509c817b79ab3ba6d74
Alex Ashitkin <alex.ashitkin@oracle.com>
2024-05-08 08:42:03 -0400

    implementation 5

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 #include <vector>
 #include <map>
 #include <unordered_map>
+#include <unordered_set>
 #include <cmath>
+#include <fstream>
+#include <chrono>
 
 using namespace std;
 
@@ -60,90 +63,57 @@ public:
 
     void AddDocument(int document_id, const string &document) {
         const vector<string> &words = SplitIntoWordsNoStop(document);
+        const auto words_size = words.size();
         document_count_++;
 
-        unordered_map<string, double> frequencies;
-//        frequencies.reserve(words.size());
-
+        unordered_map<string, double> &frequencies = doc_word_tf[document_id];
         for (const auto &word: words) {
-            if (!stop_words_.contains(word)) {
-                word_to_doc_ids[word].insert(document_id);
-                frequencies[word] += 1.0 / words.size();
-            }
+            word_to_doc_ids[word].insert(document_id);
+            frequencies[word] += 1.0 / words_size;
         }
-        doc_word_tf[document_id] = frequencies;
     }
 
     vector<Document> FindTopDocuments(const string &raw_query) const {
+
         const Query query = ParseQuery(raw_query);
         auto matched_documents = FindAllDocuments(query);
 
-        auto comp_desc = [](const Document &lhs, const Document &rhs) {
-            return lhs.relevance > rhs.relevance;
-        };
-
-        auto comp_asc = [](const Document &lhs, const Document &rhs) {
-            return lhs.relevance < rhs.relevance;
-        };
-
-        if (matched_documents.size() < MAX_RESULT_DOCUMENT_COUNT) {
-            sort(matched_documents.begin(), matched_documents.end(),
-                 comp_desc);
-            return matched_documents;
+        sort(matched_documents.begin(), matched_documents.end(),
+             [](const Document &lhs, const Document &rhs) {
+                 return lhs.relevance > rhs.relevance;
+             });
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
         }
-
-        vector<Document> result(matched_documents.begin(), matched_documents.begin() + 5);
-
-        std::make_heap(result.begin(), result.end(), comp_asc);
-
-        for (int i = 5; i < matched_documents.size(); i++) {
-            // min heap - min element at the front
-            if (matched_documents[i].relevance > result.front().relevance) {
-                // pop the head and add new element
-                std::pop_heap(result.begin(), result.end(), comp_asc);
-                result.pop_back();
-                result.push_back(matched_documents[i]);
-                // heapify
-                push_heap(result.begin(), result.end(), comp_asc);
-            }
-        }
-
-        std::sort(result.begin(), result.end(), comp_desc);
-
-//        sort(matched_documents.begin(), matched_documents.end(),
-//             [](const Document &lhs, const Document &rhs) {
-//                 return lhs.relevance > rhs.relevance;
-//             });
-//        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
-//            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
-//        }
         return matched_documents;
+
     }
 
 private:
 
     struct Query {
-        set<string> plus_words;
-        set<string> minus_words;
+        unordered_set<string> plus_words;
+        unordered_set<string> minus_words;
     };
 
     int document_count_ = 0;
     unordered_map<int, unordered_map<string, double>> doc_word_tf;
     unordered_map<string, set<int>> word_to_doc_ids;
-    set<string> stop_words_;
+    unordered_set<string> stop_words_;
 
     bool IsStopWord(const string &word) const {
-        return stop_words_.count(word) > 0;
+        return stop_words_.contains(word);
     }
 
     vector<string> SplitIntoWordsNoStop(const string &text) const {
-        vector<string> words;
-        for (const string &word: SplitIntoWords(text)) {
+        const vector<string> &all_words = SplitIntoWords(text);
+        vector<string> whitelisted;
+        for (const string &word: all_words) {
             if (!IsStopWord(word)) {
-                words.push_back(word);
+                whitelisted.push_back(word);
             }
         }
-        return words;
+        return whitelisted;
     }
 
     Query ParseQuery(const string &text) const {
@@ -161,14 +131,14 @@ private:
     }
 
     vector<Document> FindAllDocuments(const Query &query) const {
-        vector<Document> result;
 
         map<int, double> doc_to_relevance;
 
         for (const auto &pw: query.plus_words) {
-            if (word_to_doc_ids.find(pw) != word_to_doc_ids.end()) {
+            if (word_to_doc_ids.contains(pw)) {
                 const set<int> &matched_docs_ids = word_to_doc_ids.at(pw);
-                double word_idf = log(((double) document_count_) / matched_docs_ids.size());
+                const int matched_docs_size = static_cast<int>(matched_docs_ids.size());
+                double word_idf = log(((double) document_count_) / matched_docs_size);
                 for (const auto &doc_id: matched_docs_ids) {
                     double dtf = doc_word_tf.at(doc_id).at(pw);
                     doc_to_relevance[doc_id] += dtf * word_idf;
@@ -176,42 +146,81 @@ private:
             }
         }
 
+        unordered_set<int> minus_docs;
         for (const auto &mw: query.minus_words) {
-            if (word_to_doc_ids.find(mw) != word_to_doc_ids.end()) {
+            if (word_to_doc_ids.contains(mw)) {
                 const auto &matched_docs = word_to_doc_ids.at(mw);
-                for (const auto &doc_id: matched_docs) {
-                    doc_to_relevance.erase(doc_id);
-                }
+                minus_docs.insert(matched_docs.begin(), matched_docs.end());
             }
         }
 
+        for (const auto &doc_id: minus_docs) {
+            doc_to_relevance.erase(doc_id);
+        }
+
+        vector<Document> result;
         for (const auto &[id, relevance]: doc_to_relevance) {
             Document doc;
             doc.id = id;
             doc.relevance = relevance;
             result.push_back(doc);
         }
+
         return result;
     }
 };
 
 SearchServer CreateSearchServer() {
     SearchServer search_server;
-    search_server.SetStopWords(ReadLine());
 
+    search_server.SetStopWords(ReadLine());
     const int document_count = ReadLineWithNumber();
     for (int document_id = 0; document_id < document_count; ++document_id) {
         search_server.AddDocument(document_id, ReadLine());
     }
 
+//    search_server.SetStopWords("is are was a an in the with near at"s);
+//    ifstream file("./book-war-and-peace.txt");
+//
+//    cout << "reading file"s << endl;
+//    string line;
+//    int doc_counter = 0;
+//    if (file.is_open()) {
+//        while (getline(file, line)) {
+//            if (doc_counter % 1000 == 0) {
+//                cout << "indexing line "s << doc_counter << endl;
+//            }
+//            search_server.AddDocument(doc_counter++, line);
+//        }
+//        file.close();
+//    }
+
     return search_server;
 }
 
 int main() {
-    const SearchServer search_server = CreateSearchServer();
 
-    const string query = ReadLine();
-    for (const auto &[document_id, relevance]: search_server.FindTopDocuments(query)) {
+//    auto start_indexing = std::chrono::high_resolution_clock::now();
+    const SearchServer search_server = CreateSearchServer();
+//    auto elapsed_indexing = std::chrono::high_resolution_clock::now() - start_indexing;
+//    long elapsed_indexing_millis = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_indexing).count();
+
+
+//    const string query = "white horse long tail";
+    const string &query = ReadLine();
+
+//    auto start_searching = std::chrono::high_resolution_clock::now();
+
+    const vector<Document> &documents = search_server.FindTopDocuments(query);
+
+//    auto elapsed_searching = std::chrono::high_resolution_clock::now() - start_searching;
+//    long elapsed_searching_micros = std::chrono::duration_cast<std::chrono::microseconds>(elapsed_searching).count();
+//
+//    cout << "indexing: "s << elapsed_indexing_millis << " millis, searching: "s << elapsed_searching_micros
+//         << " micros"s
+//         << endl;
+
+    for (const auto &[document_id, relevance]: documents) {
         cout << "{ document_id = "s << document_id << ", "
              << "relevance = "s << relevance << " }"s << endl;
     }
