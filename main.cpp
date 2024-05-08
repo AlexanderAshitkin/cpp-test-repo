97a663e12f5403000b54e8be9740e340d680e050
Alex Ashitkin <alex.ashitkin@oracle.com>
2024-05-08 12:12:35 -0400

    cleanup

--- main.cpp
+++ main.cpp
@@ -4,11 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <unordered_map>
-#include <unordered_set>
 #include <cmath>
-#include <fstream>
-#include <chrono>
 
 using namespace std;
 
@@ -90,13 +86,13 @@ public:
 private:
 
     struct Query {
-        unordered_set<string> plus_words;
-        unordered_set<string> minus_words;
+        set<string> plus_words;
+        set<string> minus_words;
     };
 
     int document_count_ = 0;
-    unordered_map<string, unordered_map<int, double>> word_to_doc_and_tf;
-    unordered_set<string> stop_words_;
+    map<string, map<int, double>> word_to_doc_and_tf;
+    set<string> stop_words_;
 
     bool IsStopWord(const string &word) const {
         return stop_words_.contains(word);
@@ -132,7 +128,7 @@ private:
         map<int, double> doc_to_relevance;
         for (const string &plus_word: query.plus_words) {
             if (word_to_doc_and_tf.contains(plus_word)) {
-                const unordered_map<int, double> &doc_to_tf = word_to_doc_and_tf.at(plus_word);
+                const map<int, double> &doc_to_tf = word_to_doc_and_tf.at(plus_word);
                 const int total_docs_with_word = static_cast<int>(doc_to_tf.size());
                 double idf = log(((double) document_count_) / total_docs_with_word);
                 for (const auto &[doc_id, tf]: doc_to_tf) {
@@ -170,50 +166,17 @@ SearchServer CreateSearchServer() {
     for (int document_id = 0; document_id < document_count; ++document_id) {
         search_server.AddDocument(document_id, ReadLine());
     }
-
-//    search_server.SetStopWords("is are was a an in the with near at"s);
-//    ifstream file("/Users/aashitki/CLionProjects/cpp-test-repo/book-war-and-peace.txt");
-//
-//    cout << "reading file"s << endl;
-//    string line;
-//    int doc_counter = 0;
-//    if (file.is_open()) {
-//        while (getline(file, line)) {
-//            if (doc_counter % 1000 == 0) {
-//                cout << "indexing line "s << doc_counter << endl;
-//            }
-//            search_server.AddDocument(doc_counter++, line);
-//        }
-//        file.close();
-//    }
-
     return search_server;
 }
 
 int main() {
 
-//    auto start_indexing = chrono::high_resolution_clock::now();
     const SearchServer search_server = CreateSearchServer();
-//    auto elapsed_indexing = chrono::high_resolution_clock::now() - start_indexing;
-//    long elapsed_indexing_millis = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_indexing).count();
-
-
-//    const string query = "white horse long tail";
     const string &query = ReadLine();
-
-//    auto start_searching = chrono::high_resolution_clock::now();
-
     const vector<Document> &documents = search_server.FindTopDocuments(query);
-
-//    auto elapsed_searching = chrono::high_resolution_clock::now() - start_searching;
-//    long elapsed_searching_micros = std::chrono::duration_cast<chrono::microseconds>(elapsed_searching).count();
-//
-//    cout << "indexing: "s << elapsed_indexing_millis << " millis, searching: "s << elapsed_searching_micros
-//         << " micros"s
-//         << endl;
-
     for (const auto &[document_id, relevance]: documents) {
         cout << "{ document_id = "s << document_id << ", "
              << "relevance = "s << relevance << " }"s << endl;
     }
+
 }
